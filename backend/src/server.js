89c10ef805cbb7cf4ba4fa494f60f141c6122426
William Azevedo da Silva <williamsilva.uea@gmail.com>
2019-08-25 13:46:03 -0400

    add testes de web sockets

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,31 @@ const cors = require('cors'); // permite acessar a api de qualquer endereço. Li
 
 const routes = require('./router');
 
-const server = express(); // criando o servidor para receber requisições e retornar respostas
+const app = express(); // criando o servidor para receber requisições e retornar respostas
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+    console.log('Nova conexão', socket.id);
+
+    socket.on('hello', message => {
+        console.log(message)
+    });
+
+    setTimeout(() => {
+        socket.emit('world', {
+            message: 'Omnstack'
+        });
+    }, 5000);
+})
 
 mongoose.connect('mongodb+srv://will:omnistack@cluster0-94mvq.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json()); // express não usa json por default
-server.use(routes);
+app.use(cors());
+app.use(express.json()); // express não usa json por default
+app.use(routes);
 
 server.listen(3333); // porta em que o servidor vai escutar
 
--- tindev/src/pages/Main.js
+++ tindev/src/pages/Main.js
@@ -1,50 +1,94 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import { SafeAreaView, Image, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
 import dislike from '../assets/dislike.png';
 
 
-export default function Main(){
+export default function Main({ navigation }){
+    const id = navigation.getParam('user');
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers(){
+            const response = await api.get('/devs', {
+                headers: {
+                    user: id,
+                }
+            })
+
+            setUsers(response.data);
+        }
+
+        loadUsers();
+    }, [id]);
+
+    async function handleLike(){
+        const [user, ...rest] = users;
+
+        await api.post(`/devs/${user._id}/likes`, null, {
+            headers: {
+                user: id
+            },
+        })
+
+        setUsers(rest);
+    }
+
+    async function handleDislike(){
+        const [user, ...rest] = users;
+
+        await api.post(`/devs/${user._id}/dislikes`, null, {
+            headers: {
+                user: id
+            },
+        })
+
+        setUsers(rest);
+    }
+
+    async function handleLogout(){
+        await AsyncStorage.clear();
+
+        navigation.navigate('Login');
+    }
+
     return (
         <SafeAreaView style={styles.container}>
-            <Image source={logo} style={styles.logo} />
+            <TouchableOpacity onPress={handleLogout}>
+                <Image source={logo} style={styles.logo} />
+            </TouchableOpacity>
 
             <View style={styles.cardsCotainer}>
-                <View style={[styles.card, { zIndex: 3 }]}>
-                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/25959568?v=4' }} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>William A. Silva</Text>
-                        <Text style={styles.bio} numberOfLines={3}>I like good Ducks.</Text>
-                    </View>
-                </View>
-
-                <View style={[styles.card, { zIndex: 2 }]}>
-                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/25959568?v=4' }} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>William A. Silva</Text>
-                        <Text style={styles.bio} numberOfLines={3}>I like good Ducks.</Text>
-                    </View>
-                </View>
-
-                <View style={[styles.card, { zIndex: 1 }]}>
-                    <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/25959568?v=4' }} />
-                    <View style={styles.footer}>
-                        <Text style={styles.name}>William A. Silva</Text>
-                        <Text style={styles.bio} numberOfLines={3}>I like good Ducks.</Text>
-                    </View>
-                </View>
+                { users.length === 0
+                ? <Text style={styles.empty}>Acabou :(</Text> 
+                : (
+                    users.map((user, index) => (
+                        <View key={user._id} style={[styles.card, { zIndex: users.length - index }]}>
+                            <Image style={styles.avatar} source={{ uri: user.avatar }} />
+                            <View style={styles.footer}>
+                                <Text style={styles.name}>{user.name}</Text>
+                                <Text style={styles.bio} numberOfLines={3}>{user.bio}</Text>
+                            </View>
+                        </View>
+                    ))
+                )}
             </View>
             
-            <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={styles.button}>
-                    <Image source={dislike} />
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
-                    <Image source={like} />
-                </TouchableOpacity>
-            </View>
+            { users.length > 0 && (
+                <View style={styles.buttonsContainer}>
+                    <TouchableOpacity style={styles.button} onPress={handleDislike}>
+                        <Image source={dislike} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={handleLike}>
+                        <Image source={like} />
+                    </TouchableOpacity>
+                </View>
+            )}
         </SafeAreaView>
     )
 }
@@ -59,6 +103,13 @@ const styles = StyleSheet.create({
     
     logo: {
         marginTop: 30
+    },
+
+    empty: {
+        alignSelf: 'center',
+        color: '#999',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
 
     cardsCotainer: {
